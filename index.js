ed1f12e7456ccde340a31f500a321aa7eefd31bd
shreya <shreya.s@niveussolutions.com>
2023-08-16 12:07:40 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-// Set up a GET API endpoint
+// Middleware to parse JSON data in the request body
+app.use(express.json());
+
+// Sample array of data
+const dataArray = [];
+
+// Set up a GET and POST API endpoint
 app.get('/api/info/:id', (req, res) => {
   // Extract the query parameters and path parameters from the request object
   const queryParam = req.query.param;
@@ -17,7 +23,19 @@ app.get('/api/info/:id', (req, res) => {
   res.send(response);
 });
 
-const port = 3001;
+app.post('/api/data', (req, res) => {
+  // Get the JSON data from the request body
+  const jsonData = req.body;
+
+  // Add the JSON data to the array
+  dataArray.push(jsonData);
+
+  // Send the array of data as the response
+  res.json(dataArray);
+});
+
+// Start the server on port 3000
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
